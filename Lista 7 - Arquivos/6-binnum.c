6feaee1f8d7047260569160aa6f36e1690cbc13a
Rodrigo <rodrigofigsoares@gmail.com>
2025-08-11 23:51:28 -0300

    Delete Lista 7 - Arquivos directory

--- Lista 7 - Arquivos/6-binnum.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    FILE * arq = fopen("abc", "wb");
-    int p[3] = {1629495328, 1629495329, 1629495330};
-    fwrite(p, sizeof(int), 3, arq);
-    fclose(arq);
-    return 0;
-}
